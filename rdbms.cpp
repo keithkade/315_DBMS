6f413e8db922de5dbfba26c10945ae347c13aef3
Kade Keith <keithkade@aol.com>
2014-02-05 17:02:07 -0600

    shell of classes and example of accessing data

--- rdbms.cpp
+++ rdbms.cpp
@@ -1,14 +1,39 @@
 /*
 RDBMS: Kade Keith, Matthew Saari, Ryan Ledbetter, Victor Gutierrez
 */
-// this is my commit VG
 
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
 
+struct Datum{ 
+   string stringData; 
+   int numData; 
+ 
+   Datum(){}
+};
+
+struct Table{
+    vector<pair<string, vector<Datum> > > attributes;
+       
+    Table(){}
+};
+
 int main(){
   
+    //some examples of accessing the data
+    Table dummyTable;
+    vector<Datum> dummyColumn;
+    Datum dummyDatum;
+    dummyDatum.numData=10;
+    dummyColumn.push_back(dummyDatum);
+    pair<string, vector<Datum> > dummyPair;
+    dummyPair.first = "attributeName";
+    dummyPair.second = dummyColumn;
+    dummyTable.attributes.push_back(dummyPair);
+    
+    cout << dummyTable.attributes[0].second[0].numData << endl;
 }
 
