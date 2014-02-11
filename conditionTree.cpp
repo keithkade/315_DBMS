33d8cc5d2ccc2f39c4b95aa87d276659f1514b73
Matthew Saari <saari.matthew@gmail.com>
2014-02-11 01:39:19 -0600

    Update conditionTree.cpp

--- conditionTree.cpp
+++ conditionTree.cpp
@@ -1,5 +1,6 @@
+#pragma once
+#include "rdbms.h"
 #include "ConditionTree.h"
-
 
 VariableNode::VariableNode(string vn) : varName(vn){}
 Datum VariableNode::getValue(vector<string> &atribNames, vector<Datum> &relation)
@@ -11,52 +12,52 @@ Datum VariableNode::getValue(vector<string> &atribNames, vector<Datum> &relation
 			return relation[i];
 		}
 	}
+
 }
 
-
 LiteralNode::LiteralNode(string d) : data(d){}
+LiteralNode::LiteralNode(int d) : data(d){}
 Datum LiteralNode::getValue(vector<string> &atribNames, vector<Datum> &relation)
-{ 
-	return data; 
+{
+	return data;
 }
 
 OperationNode::OperationNode(OP s, LeafNode* l, LeafNode* r) : symbol(s), left(l), right(r) {}
 bool OperationNode::eval(vector<string> &atribNames, vector<Datum> &relation)
 {
 	if (left->getValue(atribNames, relation).numData != -999 && right->getValue(atribNames, relation).numData != -999)
-	{
+	{	// If both Datum nums are not -999 we know they are both representing ints and therefore can be compared.
 		switch (symbol)
 		{
 		case eq:
+			cout << (left->getValue(atribNames, relation).numData == right->getValue(atribNames, relation).numData) << "\n";
 			return left->getValue(atribNames, relation).numData == right->getValue(atribNames, relation).numData; break;
 		case neq:
 			return left->getValue(atribNames, relation).numData != right->getValue(atribNames, relation).numData; break;
 		case ls:
 			return left->getValue(atribNames, relation).numData < right->getValue(atribNames, relation).numData; break;
 		case leq:
-				return left->getValue(atribNames, relation).numData <= right->getValue(atribNames, relation).numData; break;
+			return left->getValue(atribNames, relation).numData <= right->getValue(atribNames, relation).numData; break;
 		case gr:
 			return left->getValue(atribNames, relation).numData > right->getValue(atribNames, relation).numData; break;
 		case geq:
 			return left->getValue(atribNames, relation).numData >= right->getValue(atribNames, relation).numData;
-			}
+		}
 	}
 	else if (left->getValue(atribNames, relation).numData == -999 && right->getValue(atribNames, relation).numData == -999)
-	{
+	{// If both Datum nums are -999 we know they both represent strings and therefore can be compared. However only with == and !=
 		switch (symbol)
 		{
-		case OP::eq:
+		case eq:
 			return left->getValue(atribNames, relation).stringData == right->getValue(atribNames, relation).stringData; break;
-		case OP::neq:
+		case neq:
 			return left->getValue(atribNames, relation).stringData != right->getValue(atribNames, relation).stringData; break;
 		}
 	}
-	else{
+	else{ // If we got to here then the Datums must not be of the same and therefore can not be compared so will return a false.
 		return false;
 	}
 }
-
-
 
 ComparisonNode::ComparisonNode(OperationNode* n) : operOpOperChild(n){}
 ComparisonNode::ComparisonNode(ConditionNode* n) : conditionChild(n){}
@@ -65,7 +66,7 @@ bool ComparisonNode::eval(vector<string> &atribNames, vector<Datum> &relation){
 	{
 		return operOpOperChild->eval(atribNames, relation);
 	}
-		else if (conditionChild != NULL)
+	else if (conditionChild != NULL)
 	{
 		return conditionChild->eval(atribNames, relation);
 	}
@@ -77,7 +78,7 @@ bool ComparisonNode::eval(vector<string> &atribNames, vector<Datum> &relation){
 ConjunctionNode::ConjunctionNode(vector<ComparisonNode*> cn) : compNodes(cn) {}
 bool ConjunctionNode::eval(vector<string> &atribNames, vector<Datum> &relation){
 	bool result = true;
-	for (unsigned int i = 0; i < compNodes.size(); i++)
+	for (int i = 0; i < compNodes.size(); i++)
 	{
 		result = result && compNodes[i]->eval(atribNames, relation);
 	}
@@ -88,7 +89,7 @@ bool ConjunctionNode::eval(vector<string> &atribNames, vector<Datum> &relation){
 ConditionNode::ConditionNode(vector<ConjunctionNode*> cn) : conjNodes(cn) {}
 bool ConditionNode::eval(vector<string> &atribNames, vector<Datum> &relation){
 	bool result = false;
-	for (unsigned int i = 0; i < conjNodes.size(); i++)
+	for (int i = 0; i < conjNodes.size(); i++)
 	{
 		result = result || conjNodes[i]->eval(atribNames, relation);
 	}
