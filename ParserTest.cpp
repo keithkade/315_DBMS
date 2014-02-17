b1c7894a9781116d87a43301591ff6ef0a58c2dc
Victor Gutierrez <vicfer00@gmail.com>
2014-02-17 00:25:28 -0600

    ParserTest framework
    didnt know what to write in each exactly since I really dont know what
    the output of each function is to be able to equal it to the parser
    results.

---
+++ ParserTest.cpp
@@ -0,0 +1,124 @@
+#include "stdafx.h"
+#include <iostream>
+#include "parser.h"
+#include "gtest/gtest.h"
+#include <string.h>
+#include <vector>
+#include <map>
+
+TEST(ParserTest, ParAttributeList)
+{
+
+}
+
+TEST(ParserconTest, ParCondition)
+{
+
+}
+
+TEST(ParserconjTest, ParConjunction)
+{
+
+}
+
+TEST(ParsercompTest, ParComparison)
+{
+
+}
+
+TEST(ParserexpTest, ParExpression)
+{
+
+}
+
+TEST(ParseratexpTest, ParAtomExpression)
+{
+
+}
+
+TEST(ParserselTest, ParSelection)
+{
+
+}
+
+TEST(ParserprojTest, ParProjection)
+{
+
+}
+
+TEST(ParserRenTest, ParRenaming)
+{
+
+}
+
+TEST(ParserMyUnionTest, ParmyUnion)
+{
+
+}
+
+TEST(ParserDiffTest, ParDifference)
+{
+
+}
+
+TEST(ParserProductTest, ParProduct)
+{
+
+}
+
+TEST(ParserNatJoinTest, ParNaturalJoin)
+{
+
+}
+
+TEST(ParserQueryTest, ParQuery)
+{
+
+}
+
+TEST(ParserOpenTest, ParOpen)
+{
+
+}
+
+TEST(ParserCloseTest, ParClose)
+{
+
+}
+
+TEST(ParserWriteTest, ParWrite)
+{
+
+}
+
+TEST(ParserShowTest, ParShow)
+{
+
+}
+
+TEST(ParserCreateTest, ParCreate)
+{
+
+}
+
+TEST(ParserUpdateTest, ParUpdate)
+{
+
+}
+
+TEST(ParserInsertTest, ParInsert)
+{
+
+}
+
+TEST(ParserDeleteTest, ParmyDelete)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	RUN_ALL_TESTS();
+	std::getchar(); // keep console window open until Return keystroke
+}
